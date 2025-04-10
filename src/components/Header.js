164b50d1c67ee35bab008073a0ff2e33322d6ac8
Sladey01 <103081216+Sladey01@users.noreply.github.com>
2025-04-10 12:31:26 +0100

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header-content">
+        <div className="logo">
+          <span className="logo-snyk">Snyk</span>&nbsp;Best Practices Guide
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
